041bd80204a4f2c5041c9a8b3a16efefaac69a26
Daniel Voth <danielglennvoth@gmail.com>
2014-07-26 16:12:08 -0500

    Edited custom unity splash screen, added experimental scripts.

---
+++ Assets/Scripts/TimeLose.js
@@ -0,0 +1,15 @@
+﻿     var toggleGUI : boolean;
+     
+    function OnTriggerEnter (other : Collider) {
+    //if (other.gameObject.CompareTag("Box"))
+    toggleGUI = true;
+    }
+     
+    //function OnTriggerExit (other : Collider) {
+    //toggleGUI = false;
+    //}
+     
+    function OnGUI () {
+    if (toggleGUI == true)
+    GUI.Label (Rect (10, 300, 100, 20), "You Lose");
+    }
