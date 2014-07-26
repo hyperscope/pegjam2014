da5d25217b5daa6a3075f5ef7645b0a4ee1cc92d
Daniel Voth <danielglennvoth@gmail.com>
2014-07-25 21:01:48 -0500

    Unity work so far

---
+++ Assets/Scripts/followthelight.js
@@ -0,0 +1,30 @@
+﻿    var target : Transform;
+    var smoothTime = 0.3;
+    var xOffset : float = 1.0;
+    var zOffset : float = 1.0;
+ 
+    private var thisTransform : Transform;
+    private var velocity : Vector2;
+ 
+ 
+    function Start()
+    {
+           thisTransform = transform;
+    }
+ 
+    function LateUpdate()
+    {
+ 
+           thisTransform.position.x = Mathf.Lerp( thisTransform.position.x, target.position.x + xOffset, Time.deltaTime * smoothTime);
+ 
+           thisTransform.position.z = Mathf.Lerp( thisTransform.position.z, target.position.z + zOffset, Time.deltaTime * smoothTime);
+ 
+    
+    //function OnTriggerStay (other : Collider){
+	//if (other.gameObject.CompareTag("Black Orbit"))
+		//{
+		//damaging = true;
+		//playerHealth -= 1; //Time.deltaTime;
+		//}
+    
+    }
